9f8246410f29266d2bc305eabf0bec66f3c0f3c3
Alexander Hill <CodeBreadpudding@gmail.com>
2021-07-13 20:36:37 -0400

    Got Fabric to notice me

---
+++ src/main/java/dev/breadpudding/altoidstin/AltoidsTin.java
@@ -0,0 +1,10 @@
+package dev.breadpudding.altoidstin;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AltoidsTin implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // ...
+    }
+}
